ad07a8f1b8ca45b37b8f89221396276aa59dd4d8
EpicWolverine <epicwolverine@me.com>
2016-10-09 10:34:08 -0400

    Added hacky GET replacement for PUT

--- Ocupado-WebServer/Ocupado-WebServer/Controllers/SensorUpdateController.cs
+++ Ocupado-WebServer/Ocupado-WebServer/Controllers/SensorUpdateController.cs
@@ -8,8 +8,23 @@ using System.Net.Http;
 using System.Web.Http;
 
 namespace Ocupado_WebServer.Controllers {
+    [RoutePrefix("api/SensorUpdate")]
     public class SensorUpdateController : ApiController {
+
+        [HttpPut]
+        [Route("StallStatus")]
         public Stall PutStallStatus(Stall status) {
+            Stall.UpdateStatus(status); //store update
+            BathroomInfoController.bathrooms = Bathroom.LoadAll(); //reload bathroom data
+            return status;
+        }
+
+        [HttpGet]
+        [Route("StallStatusGet")]
+        public Stall StallStatusGet(int id, bool occupied) {
+            Stall status = new Stall();
+            status.id = id;
+            status.occupied = occupied;
             Stall.UpdateStatus(status); //store update
             BathroomInfoController.bathrooms = Bathroom.LoadAll(); //reload bathroom data
             return status;
